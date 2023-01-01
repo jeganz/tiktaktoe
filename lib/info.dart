330848cd0a3d07aaacfc54bfef44ad26c4d7cd85
jeganz <jeganz2255@gmail.com>
2023-01-01 15:11:03 +0530

    link aded

--- lib/info.dart
+++ lib/info.dart
@@ -5,6 +5,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class about extends StatelessWidget {
   const about({super.key});
@@ -46,21 +47,47 @@ class about extends StatelessWidget {
             Row(children: [
               IconButton(
                   iconSize: 30,
-                  onPressed: (() {}),
+                  onPressed: (() async {
+                    final Uri _url = Uri.parse('mailto:jeganz2255@gmail.com');
+                    if (!await launchUrl(_url)) {
+                      throw 'Could not launch $_url';
+                    }
+                  }),
                   icon: FaIcon(FontAwesomeIcons.envelope)),
               IconButton(
                   iconSize: 30,
-                  onPressed: (() {}),
+                  onPressed: (() async {
+                    final Uri _url =
+                        Uri.parse('https://www.instagram.com/jegan._.s/');
+                    if (!await launchUrl(_url)) {
+                      throw 'Could not launch $_url';
+                    }
+                  }),
                   icon: FaIcon(FontAwesomeIcons.instagram)),
               IconButton(
                   iconSize: 30,
-                  onPressed: (() {}),
+                  onPressed: (() async {
+                    final Uri _url =
+                        Uri.parse('https://www.linkedin.com/in/jeganshibu');
+                    if (!await launchUrl(_url)) {
+                      throw 'Could not launch $_url';
+                    }
+                  }),
                   icon: FaIcon(FontAwesomeIcons.linkedin)),
               IconButton(
                   iconSize: 30,
-                  onPressed: (() {}),
+                  onPressed: (() async {
+                    final Uri _url = Uri.parse('https://github.com/jeganz');
+                    if (!await launchUrl(_url)) {
+                      throw 'Could not launch $_url';
+                    }
+                  }),
                   icon: FaIcon(FontAwesomeIcons.github)),
-            ])
+            ]),
+            Text(
+              'this is a limited edition app \nfor updates contact developer\nexpecting valuable feedbacks',
+              style: st2,
+            )
           ],
         ),
       ),
